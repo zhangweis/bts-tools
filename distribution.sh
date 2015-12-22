5da9fc8a43607f1f5f40b001c758bbc4d7cd8a55
zhangwei <cdzhangw@gmail.com>
2015-12-22 15:48:51 +0800

    distribution.sh

---
+++ distribution.sh
@@ -0,0 +1,2 @@
+curl -s http://richlist.btsgame.org/botfund.json | ./asset-holders.js | while read p; do printf "$p:123\n"; done | ./dividends.js --profit $2 | awk -v from="$1" -F":" '{print "transfer "from" "$1" "$2 " CNY 0 true"}'
+
