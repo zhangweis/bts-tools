b2cd13fe76987bf6cdcf4d1abaa1469b0d481130
zhangwei <cdzhangw@gmail.com>
2015-12-22 17:20:18 +0800

    distribution.sh

--- get-balance.js
+++ get-balance.js
@@ -4,7 +4,7 @@ var WebSocket = require('ws');
 var ws = new WebSocket('wss://bitshares.dacplay.org/ws');
  
 ws.on('open', function open() {
-  ws.send(JSON.stringify({"method": "call", "params": [0, "get_named_account_balances", [process.argv[2], ["1.3.289"]]], "id": 1}));
+  ws.send(JSON.stringify({"method": "call", "params": [0, "get_named_account_balances", [process.argv[2], [process.argv[3]]]], "id": 1}));
 });
  
 ws.on('message', function(data, flags) {
